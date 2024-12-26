bfac97d96519b97bd6bb4aaa9872567255f26f91
anmulll <anmulll29@gmail.com>
2024-12-26 20:15:02 +0800

    first commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        return view("profile");
+    }
+
+    public function profile($nama)
+    {
+        $nilai = 80;
+        // $nilaiArray = ['80','90']; // array biasa
+        $nilaiArray = [
+            [
+                'nama' => 'agus',
+                'nilai' => 50
+            ],
+            [
+                'nama' => 'Ilham',
+                'nilai' => 80
+            ]
+        ];
+        return view("profile",compact('nama','nilai', 'nilaiArray'));
+    }
+}
